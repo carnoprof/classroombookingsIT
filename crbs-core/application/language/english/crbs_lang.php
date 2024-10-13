70fd3d8e6fca9290193d13e9bf640e52767050be
Yu-sudo-coder <congiafabrizio@asproni-fermi.edu.it>
2024-10-13 16:12:52 +0200

    Update crbs_lang.php
    Aggiunte altre traduzioni

--- crbs-core/application/language/english/crbs_lang.php
+++ crbs-core/application/language/english/crbs_lang.php
@@ -1,15 +1,15 @@
 <?php
 
-$lang['crbs_mustbeloggedin'] = 'You must be logged in to access this page.';
-$lang['crbs_mustbeadmin'] = 'You must be an Administrator to access this page.';
+$lang['crbs_mustbeloggedin'] = 'Devi aver effettuato il login per accedere a questa pagina.';
+$lang['crbs_mustbeadmin'] = 'Per accedere a questa pagina devi essere un amministratore.';
 
-$lang['crbs_action_dberror'] = 'A database error occured while %s this item.';
-$lang['dberror'] = 'A database error occured while %s the %s.';
+$lang['crbs_action_dberror'] = 'Si è verificato un errore nel database durante %s this item.';
+$lang['dberror'] = 'Si è verificato un errore nel database durante %s the %s.';
 $lang['crbs_action_added'] = '%s aggiunta correttamente.';
 $lang['crbs_action_saved'] = '%s salvata correttamente.';
 $lang['crbs_action_modified'] = '%s modificata correttamente.';
 $lang['crbs_action_deleted'] = 'Cancellazione avvenuta con successo.';
 
-$lang['colour_invalid'] = 'You entered an invalid colour value.';
+$lang['colour_invalid'] = 'Inserito un colore non valido';
 
 ?>
