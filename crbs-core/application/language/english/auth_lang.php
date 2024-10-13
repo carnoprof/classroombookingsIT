d4c836b5d3043553686aed699e7a1dcf1e5412cc
Yu-sudo-coder <congiafabrizio@asproni-fermi.edu.it>
2024-10-13 16:21:23 +0200

    Update auth_lang.php

--- crbs-core/application/language/english/auth_lang.php
+++ crbs-core/application/language/english/auth_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['auth_ldap_no_server_or_port'] = 'No server and/or port supplied.';
-$lang['auth_ldap_no_socket_connection'] = 'Connection error or timed out.';
-$lang['auth_ldap_invalid_ldap_uri'] = 'Invalid LDAP connection URI.';
-$lang['auth_ldap_no_username_or_password'] = 'No username and/or password.';
-$lang['auth_ldap_bind_error'] = 'LDAP bind error or bad username and/or password.';
-$lang['auth_ldap_search_error'] = 'LDAP search error.';
-$lang['auth_ldap_search_num_results_error'] = 'LDAP search did not return exactly one result.';
-$lang['auth_ldap_search_get_entry_error'] = 'LDAP get search result entry error.';
-$lang['auth_ldap_search_get_attributes_error'] = 'LDAP get user attributes error.';
+$lang['auth_ldap_no_server_or_port'] = 'Nessun server e/o porta forniti.';
+$lang['auth_ldap_no_socket_connection'] = 'Errore di connessione o timeout.';
+$lang['auth_ldap_invalid_ldap_uri'] = 'URI di connessione LDAP non valido.';
+$lang['auth_ldap_no_username_or_password'] = 'Nessun nome utente e/o password forniti.';
+$lang['auth_ldap_bind_error'] = 'Errore di bind LDAP o nome utente e/o password errati.';
+$lang['auth_ldap_search_error'] = 'Errore nella ricerca LDAP.';
+$lang['auth_ldap_search_num_results_error'] = 'La ricerca LDAP non ha restituito esattamente un risultato.';
+$lang['auth_ldap_search_get_entry_error'] = 'Errore nell’ottenere l’entry dei risultati della ricerca LDAP.';
+$lang['auth_ldap_search_get_attributes_error'] = 'Errore nell’ottenere gli attributi utente LDAP.';
